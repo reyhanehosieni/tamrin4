f74c3ada6012902a0c5252e0da687bb6402dd19e
reyhanehosieni <92735882+reyhanehosieni@users.noreply.github.com>
2021-11-03 13:22:01 +0330

    Add files via upload

---
+++ t4s3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+
+	int a, b, max;
+	cout << "adad avval ra vared konid : ";
+	cin >> a;
+	cout << "adad dovvom ra vared konid : ";
+	cin >> b;
+
+	if (a > b) max = a;
+	else max = b;
+
+	while (true) {
+
+		if (max % a == 0 && max % b == 0) break;
+		else ++max;
+	}
+
+	cout << "K.M.M do adad vared shode barabar ast ba :" << max;
+
+	return 0;
+}
